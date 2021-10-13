538bf2217db0c695338c2eb0995fc39bbe2e95a8
ASlugin <slugin.alexander@gmail.com>
2021-10-13 15:18:00 +0300

    Add tests and renamed function

--- Homework 14.09.21/4. HalfQSort/4. HalfQSort.c
+++ Homework 14.09.21/4. HalfQSort/4. HalfQSort.c
@@ -1,8 +1,9 @@
 ﻿#include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <time.h>
 
-void elementInPlace(int* array, int amount)
+void firstElementInPlace(int* array, int amount)
 {
     int left = 1;
     int right = amount - 1;
@@ -28,11 +29,45 @@ void elementInPlace(int* array, int amount)
     array[0] = temp;
 }
 
+bool tests()
+{
+    #define AMOUNTTESTS 3
+    int array[AMOUNTTESTS][10] = 
+    {
+        {79, 87, 26, 29, 7, 12, 43, 51, 65, 22},
+        {35, 69, 5, 22, 15, 31, 19, 23, 18, 8},
+        {58, 4, 82, 25, 81, 50, 6, 0, 27, 8}
+    };
+    const int result[AMOUNTTESTS][10] =
+    {
+        {65, 22, 26, 29, 7, 12, 43, 51, 79, 87},
+        {18, 8, 5, 22, 15, 31, 19, 23, 35, 69},
+        {0, 4, 8, 25, 27, 50, 6, 58, 81, 82}
+    };
+    for (int i = 0; i < AMOUNTTESTS; ++i)
+    {
+        firstElementInPlace(array[i], 10);
+        for (int j = 0; j < 10; ++j)
+        {
+            if (array[i][j] != result[i][j])
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
 int main()
 {
+    if (!tests())
+    {
+        printf("%s", "Tests failed!");
+        return 0;
+    }
     srand(time(NULL));
-#define SIZE 10
-    int array[SIZE] = { 0 };
+    #define SIZE 10
+    int array[SIZE] = {0};
     for (int i = 0; i < SIZE; ++i)
     {
         array[i] = rand() % 100;
@@ -40,7 +75,7 @@ int main()
     }
     printf("\n");
 
-    elementInPlace(array, SIZE);
+    firstElementInPlace(array, SIZE);
     for (int i = 0; i < SIZE; ++i)
     {
         printf("%d ", array[i]);
