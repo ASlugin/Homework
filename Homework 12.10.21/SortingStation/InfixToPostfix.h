9f47aadebd30ad0fb2cf970093f5e0978cb40fe2
ASlugin <slugin.alexander@gmail.com>
2021-10-25 21:13:02 +0300

    fix character

--- Homework 12.10.21/SortingStation/InfixToPostfix.h
+++ Homework 12.10.21/SortingStation/InfixToPostfix.h
@@ -1,6 +1,6 @@
 #pragma once
 
-// Ñonverts from infix notation to postfix notation
+// Converts from infix notation to postfix notation
 // Returns error code:
 //  0 - successfully
 // -1 - unacceptable characters
