f31b1c214421e42e3e729507131c23df174352d3
ASlugin <slugin.alexander@gmail.com>
2021-11-14 00:05:10 +0300

    Add home task Parse tree

---
+++ Homework 26.10.21/ParseTree/ParseTree.c
@@ -0,0 +1,26 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include "Tree.h"
+#include "Test.h"
+
+int main()
+{
+    if (!areTestPassing())
+    {
+        printf("Test failed!");
+        return -1;
+    }
+
+    Tree* tree = createTreeAndReadFromFile("Input.txt");
+    if (tree == NULL)
+    {
+        return -1;
+    }
+    printTree(tree);
+    
+    const int result = calculate(tree);
+    printf("%d", result);
+
+    deleteTree(&tree);
+    return 0;
+}
