ee1b1ef65cb4213e3382fe79ca845f666a78a971
ASlugin <slugin.alexander@gmail.com>
2021-12-14 12:51:31 +0300

    Add problem 2 (only function Create array)

---
+++ Зачёт/problem2/TwoDimensionalArray.h
@@ -0,0 +1,6 @@
+#pragma once
+
+typedef struct TwoDimensionalArray TwoDimensionalArray;
+
+// Creates two-dimensional array and returns pointer to array
+TwoDimensionalArray* createArray(int numberOfRows, int numberOfColumns);
---
+++ Зачёт/problem2/problem2.c
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include "TwoDimensionalArray.h"
+#include <stdio.h>
+
+int main(void)
+{
+    int numberOfRows = 0;
+    int numberOfColumns = 0;
+    printf("Enter number of rows: ");
+    scanf("%d", &numberOfRows);
+    printf("Enter number of columns: ");
+    scanf("%d", &numberOfColumns);
+    
+    TwoDimensionalArray* array = createArray(numberOfRows, numberOfColumns);
+
+
+    return 0;
+}
